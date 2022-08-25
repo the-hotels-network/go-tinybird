ed8451982c7e698a8064de0b006ff8a63ebc00fa
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2022-08-25 21:24:42 +0200

    feat(env) add test.

---
+++ internal/env/main_test.go
@@ -0,0 +1,35 @@
+package env_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/the-hotels-network/go-tinybird/internal/env"
+)
+
+func TestGet(t *testing.T) {
+	e := env.Get("TB_TEST_GET", "a")
+	assert.Equal(t, e, "a")
+
+	t.Setenv("TB_TEST_GET", "b")
+	e = env.Get("TB_TEST_GET", "a")
+	assert.Equal(t, e, "b")
+}
+
+func TestGetBool(t *testing.T) {
+	e := env.GetBool("TB_TEST_GET", false)
+	assert.Equal(t, e, false)
+
+	t.Setenv("TB_TEST_GET", "true")
+	e = env.GetBool("TB_TEST_GET", false)
+	assert.Equal(t, e, true)
+}
+
+func TestGetInt(t *testing.T) {
+	e := env.GetInt("TB_TEST_GET", 1)
+	assert.Equal(t, e, 1)
+
+	t.Setenv("TB_TEST_GET", "3")
+	e = env.GetInt("TB_TEST_GET", 1)
+	assert.Equal(t, e, 3)
+}
