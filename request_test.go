e3849dffcd24fa1348a73cb29b4e9db4af0952f6
Gabriel Browarnik <gabrielbrowar@gmail.com>
2023-04-28 15:57:10 -0300

    adding custom base url on each request if needed

--- request_test.go
+++ request_test.go
@@ -34,3 +34,30 @@ func TestRequest(t *testing.T) {
 	assert.Equal(t, res.Rows, uint(1))
 	assert.Equal(t, res.Data, []tinybird.Row{{"Col1": "1", "Col2": float64(2)}})
 }
+
+func TestRequestWithCustomURL(t *testing.T) {
+	req := tinybird.Request{
+		Method: http.MethodGet,
+		Pipe: tinybird.Pipe{
+			Name: "test",
+			URL:  "https://api.us-east.tinybird.co/v0/pipes",
+			Workspace: tinybird.Workspace{
+				Name: "test",
+			},
+		},
+	}
+
+	tinybird.MockResponse(
+		http.StatusOK,
+		`{"data":[{"Col1": "1", "Col2": 2}],"rows":1,"statistics":{"elapsed":0.00091042,"rows_read": 4,"bytes_read": 296}}`,
+	)
+
+	req.Execute()
+	res := req.Response
+
+	assert.Equal(t, req.URL(), "https://api.us-east.tinybird.co/v0/pipes/test.json")
+	assert.Nil(t, req.Error)
+	assert.Equal(t, res.Status, http.StatusOK)
+	assert.Equal(t, res.Rows, uint(1))
+	assert.Equal(t, res.Data, []tinybird.Row{{"Col1": "1", "Col2": float64(2)}})
+}
